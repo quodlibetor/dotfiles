e01872f2cf0be1e8671d8d9cc5ebf154d06b5bd1
Brandon W Maister <quodlibetor@gmail.com>
2025-01-30 15:46:08 +0000

    import some fixes

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- from https://stackoverflow.com/a/8585343/25616
+map("n", "<leader>bq", ":bp<bar>sp<bar>bn<bar>bd<CR>", { desc = "Close buffer while keeping window" })

c67bf8605642bcfc81e3b09bbf5307cf660a1049
Brandon W Maister <quodlibetor@gmail.com>
2025-09-17 17:01:08 +0000

    move several installs from brew to mise
    slightly faster to install, hopefully

--- install.sh
+++ install.sh
@@ -7,27 +7,27 @@ script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 # install package-manager-provided things
 bash "${script_dir}/install-base" || true
 
-if ! chezmoi="$(command -v chezmoi)"; then
-    bin_dir="${HOME}/.local/bin"
-    chezmoi="${bin_dir}/chezmoi"
-    echo "Installing chezmoi to '${chezmoi}'" >&2
-    if command -v curl >/dev/null; then
-        chezmoi_install_script="$(curl -fsSL https://chezmoi.io/get)"
-    elif command -v wget >/dev/null; then
-        chezmoi_install_script="$(wget -qO- https://chezmoi.io/get)"
-    else
-        echo "To install chezmoi, you must have curl or wget installed." >&2
-        exit 1
-    fi
-    sh -c "${chezmoi_install_script}" -- -b "${bin_dir}"
-    unset chezmoi_install_script bin_dir
-fi
+# if ! chezmoi="$(command -v chezmoi)"; then
+#     bin_dir="${HOME}/.local/bin"
+#     chezmoi="${bin_dir}/chezmoi"
+#     echo "Installing chezmoi to '${chezmoi}'" >&2
+#     if command -v curl >/dev/null; then
+#         chezmoi_install_script="$(curl -fsSL https://chezmoi.io/get)"
+#     elif command -v wget >/dev/null; then
+#         chezmoi_install_script="$(wget -qO- https://chezmoi.io/get)"
+#     else
+#         echo "To install chezmoi, you must have curl or wget installed." >&2
+#         exit 1
+#     fi
+#     sh -c "${chezmoi_install_script}" -- -b "${bin_dir}"
+#     unset chezmoi_install_script bin_dir
+# fi
 
 set -- init --apply --no-tty --no-pager --keep-going --source="${script_dir}"
 
 echo "Running 'chezmoi $*'" >&2
-"$chezmoi" "$@" || true
+mise exec -- chezmoi "$@" || true
 
-if [ -e $HOME/init.sh ]; then
+if [ -e "$HOME/init.sh" ]; then
     "$HOME/init.sh"
 fi

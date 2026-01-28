84674dfdf2d0edcc369cd69070e2e5f03146dfb5
Brandon W Maister <quodlibetor@gmail.com>
2026-01-28 21:56:57 +0000

    updates

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -20,3 +20,11 @@ vim.api.nvim_create_autocmd('FileType', {
     })
   end,
 })
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPost" }, {
+  group = vim.api.nvim_create_augroup('lsp_hacks', { clear = true }),
+  pattern = ".env*",
+  callback = function(e)
+    vim.diagnostic.enable(false, { bufnr = e.buf })
+  end,
+})

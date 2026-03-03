9f9e4c4c14eedcbff47163953de800af129d0498
Brandon W Maister <quodlibetor@gmail.com>
2026-03-03 13:51:49 +0000

    improve jjcd script

--- dot_config/zsh/executable_jjcd.zsh
+++ dot_config/zsh/executable_jjcd.zsh
@@ -5,7 +5,23 @@
 
 __jjcd_pick() {
   local items count height
-  items=$(jj workspace list)
+  items=$(jj workspace list --template '
+self.name() ++ ": " ++
+self.target().change_id().shortest(4) ++ " " ++
+self.target().commit_id().shortest(4) ++ " " ++
+if(self.target().empty(), "(empty) ", "") ++
+coalesce(
+  self.target().bookmarks().map(|b| b.name()).join(","),
+  self.target().parents().map(|p| p.bookmarks().map(|b| b.name()).join(",")).join(" "),
+  self.target().parents().map(|p| p.parents().map(|g| g.bookmarks().map(|b| b.name()).join(",")).join(" ")).join(" "),
+  "(no bookmark)"
+) ++ " | " ++
+coalesce(
+  self.target().description().first_line(),
+  self.target().parents().map(|p| coalesce(p.description().first_line(), p.parents().map(|g| g.description().first_line()).join("; "))).join("; "),
+  "(no description)"
+) ++ "\n"
+')
   count=$(echo "$items" | wc -l)
   height=$(( count < 10 ? count +2 : 12 ))
   echo "$items" | fzf --prompt="workspace> " --height=$height | awk -F': ' '{print $1}'
